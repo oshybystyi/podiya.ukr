a4df4893539e7134ec8e83692cde7e436939fa2f
Oleksandr Shybystyi <oleksandr.shybystyi@gmail.com>
2015-05-29 12:35:34 +0300

    When click on colorbox image on event page - hide the colorbox

--- assets/javascripts/event-page.js
+++ assets/javascripts/event-page.js
@@ -4,9 +4,15 @@
     $('a.lightbox').colorbox({
         maxHeight: '90%', // resize for viewport
         maxWidth: '90%', // resize for viewport
-        closeButton: false // hide close button
-        //overlayClose: true,
-        //opacity: 0.5
+        closeButton: false, // hide close button
+        onComplete: function() {
+            $('#colorbox:not(.close-event-attached)')
+                .bind('click', function(ev) {
+                    ev.preventDefault();
+                    $('a.lightbox').colorbox.close();
+                })
+                .addClass('close-event-attached');
+        }
     });
 
 })(jQuery, window);
